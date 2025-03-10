ec302045db9000bee8b7632dbaa975fd178d896e
ayush-17-08 <ayushabhinav812@gmail.com>
2025-03-10 23:30:08 +0530

    Kafka producer

--- src/main/java/com/infinity/commerce/smart_vyapar/kafka/TransactionProducer.java
+++ src/main/java/com/infinity/commerce/smart_vyapar/kafka/TransactionProducer.java
@@ -1,17 +1,16 @@
 package com.infinity.commerce.smart_vyapar.kafka;
 
 import com.infinity.commerce.smart_vyapar.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 //why?
 public class TransactionProducer {
-    private final KafkaTemplate<String, TransactionEvent> kafkaTemplate;
 
-    public TransactionProducer(KafkaTemplate<String, TransactionEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    @Autowired
+    private KafkaTemplate<String, TransactionEvent> kafkaTemplate;
 
     public void sendEvent(String topic, TransactionEvent event) {
         kafkaTemplate.send(topic, event);
